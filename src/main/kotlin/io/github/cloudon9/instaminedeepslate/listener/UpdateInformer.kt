b43562f8eb5f619e3e2c2ee632df03aab5c819c6
CloudOn9 <thealtfulthing@tutanota.com>
2022-02-03 21:32:06 -0800

    Add update checker.

---
+++ src/main/kotlin/io/github/cloudon9/instaminedeepslate/listener/UpdateInformer.kt
@@ -0,0 +1,21 @@
+package io.github.cloudon9.instaminedeepslate.listener
+
+import io.github.cloudon9.instaminedeepslate.InstaMineDeepslate
+import net.kyori.adventure.text.minimessage.MiniMessage
+import org.bukkit.configuration.file.FileConfiguration
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerJoinEvent
+
+class UpdateInformer(private val config: FileConfiguration,
+                     private val plugin: InstaMineDeepslate): Listener {
+
+    @EventHandler
+    fun informUpdate(e: PlayerJoinEvent) {
+        if (config.getBoolean("updateCheck") && plugin.updateFound) {
+            e.player.sendMessage(MiniMessage.get().parse(
+                config.getString("message.miniMessage.updateFound")!!)
+            )
+        }
+    }
+}
